1fb1508ef6dfccc4dde442170f5a68f1b8fd0eea
Kelemen Florin <kelemendorianflorin0@gmail.com>
2023-12-18 19:25:15 +0200

    todo check if user is student or librarian

---
+++ LibraryManagementSystem/ViewModel/LibrarianViewModel.cs
@@ -0,0 +1,71 @@
+﻿using LibraryManagementSystem.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace LibraryManagementSystem.ViewModel
+{
+    public class LibrarianViewModel : ViewModelBase
+    {
+        private ViewModelBase _currentChildView;
+
+        public ICommand ShowAllStockViewCommand { get; }
+        public ICommand ShowModifyStockViewCommand { get; }
+        public ICommand ShowAddBooksViewCommand { get; }
+        public ICommand ShowManageAccountsViewCommand { get; }
+        public ICommand ShowLibraryStatisticsViewCommand { get; }
+        public ICommand ShowLogoutViewCommand { get; }
+
+        public ViewModelBase CurrentChildView
+        {
+            get { return _currentChildView; }
+            set { _currentChildView = value; OnPropertyChanged(nameof(CurrentChildView)); }
+        }
+
+        public LibrarianViewModel()
+        {
+            ShowAllStockViewCommand = new ViewModelCommand(ExecuteShowAllStockViewCommand);
+            ShowModifyStockViewCommand = new ViewModelCommand(ExecuteShowModifyStockViewCommand);
+            ShowAddBooksViewCommand = new ViewModelCommand(ExecuteShowAddBooksViewCommand);
+            ShowManageAccountsViewCommand = new ViewModelCommand(ExecuteShowManageAccountsViewCommand);
+            ShowLibraryStatisticsViewCommand = new ViewModelCommand(ExecuteShowLibraryStatisticsViewCommand);
+            ShowLogoutViewCommand = new ViewModelCommand(ExecuteShowLogoutViewCommand);
+
+            //to change
+            ExecuteShowLogoutViewCommand(null);
+        }
+
+        private void ExecuteShowLibraryStatisticsViewCommand(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ExecuteShowLogoutViewCommand(object obj)
+        {
+            CurrentChildView = new LogoutViewModel();
+        }
+
+        private void ExecuteShowManageAccountsViewCommand(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ExecuteShowAddBooksViewCommand(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ExecuteShowModifyStockViewCommand(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ExecuteShowAllStockViewCommand(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
